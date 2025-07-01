58370e89ec93256fcc1e25eb908707b43962ee17
Aditya Gupta <161147051+Gytondy@users.noreply.github.com>
2025-07-01 15:10:18 +0000

    Datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS COde as Newer version
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3) // code readability should br high
+
+console.log("Aditya")
+
+let name = "Aditya"
+let age = 20
+let isloggedIn = false
+let state; 
+
+// number => 2 to the power 53
+// bigint
+// string => ""
+// boolean = true/false 
+// null => standlone value
+// undefined => 
+// symbol = unique ness
+
+
+// object
+
+console.log(typeof null)
